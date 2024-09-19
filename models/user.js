171a880adf71d258019a0e785af08d56be4784e8
satvik6392 <trivedisatvik2004@gmail.com>
2024-09-19 15:35:59 +0530

    indexing issue resolved

--- models/user.js
+++ models/user.js
@@ -4,7 +4,11 @@ const bcrypt = require('bcrypt');
 
 
 const User = sequelize.define('User', {
-  
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
     first_name: {
         type: DataTypes.STRING,
         allowNull: false
@@ -23,8 +27,6 @@ const User = sequelize.define('User', {
     },
     email: {
         type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
     },
     password: {
         type: DataTypes.STRING,
